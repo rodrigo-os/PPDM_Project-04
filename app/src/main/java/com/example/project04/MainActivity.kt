63638e9a9da0096f73eed350939de5e2e791747c
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2022-05-08 20:26:17 -0300

    Parte 02 - Aula 06/05

--- app/src/main/java/com/example/project04/MainActivity.kt
+++ app/src/main/java/com/example/project04/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.project04
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,11 +11,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.project04.taskscreen.TaskScreen
+import com.example.project04.taskscreen.TaskViewModel
+import com.example.project04.taskscreen.TaskViewModelFactory
 import com.example.project04.ui.theme.Project04Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val taskViewModel: TaskViewModel by viewModels<TaskViewModel> {
+            TaskViewModelFactory(
+                (this.applicationContext as TodoApplication).database.taskDao()
+            )
+        }
         setContent {
             Project04Theme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    TaskScreen(taskViewModel)
                 }
             }
         }
